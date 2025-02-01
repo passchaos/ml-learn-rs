5bf172268131a082c0c2428c93b5ecb1666c436a
passchaos <greedwolf.dss@gmail.com>
2025-02-01 16:51:01 +0800

    feat: support dynamic dimention data set, add basic knn visualization

--- src/knn/mod.rs
+++ src/knn/mod.rs
@@ -1,18 +1,23 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    io::{BufRead, BufReader},
+};
 
-use crate::{Data, Label, SampleData};
+use anyhow::Result;
 
-pub fn classify(input: Data, sample_datas: &[SampleData], k: usize) -> Label {
+use crate::{Data, SampleData};
+
+pub fn classify(input: Data, sample_datas: &[SampleData], k: usize) -> String {
     let mut data_info: Vec<_> = sample_datas
         .iter()
-        .map(|s_d| (s_d.data.distance(&input), s_d.label))
+        .map(|s_d| (s_d.data.distance(&input), s_d.label.to_string()))
         .collect();
 
     println!("data info: {data_info:?}");
     data_info.sort_by(|a, b| a.0.total_cmp(&b.0));
     println!("sorted data info: {data_info:?}");
 
-    let mut results: HashMap<Label, u32> = HashMap::new();
+    let mut results: HashMap<String, u32> = HashMap::new();
 
     data_info.into_iter().take(k).for_each(|(_value, label)| {
         *results.entry(label).or_default() += 1;
@@ -24,4 +29,34 @@ pub fn classify(input: Data, sample_datas: &[SampleData], k: usize) -> Label {
         .expect("can't get max result");
 
     result.0
+}
+
+pub fn file2matrix(file_path: &str) -> Result<Vec<SampleData>> {
+    let file = std::fs::File::open(file_path).unwrap();
+    let file = BufReader::new(file);
+
+    let mut result = vec![];
+
+    for line in file.lines() {
+        let line = line?;
+        let mut data: Vec<_> = line.split('\t').take(4).collect();
+
+        let label = data
+            .pop()
+            .ok_or(anyhow::anyhow!("line has no 4th content: {data:?}"))?
+            .to_string();
+
+        let mut inner = vec![];
+        for d in data {
+            let v = d.trim().parse()?;
+            inner.push(v);
+        }
+
+        result.push(SampleData {
+            data: Data { inner },
+            label,
+        });
+    }
+
+    Ok(result)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,82 @@
+use anyhow::Result;
+use plotly::{
+    Plot, Scatter,
+    common::{Marker, Mode},
+};
+
 mod knn;
 
+#[derive(Debug)]
 struct Data {
-    a: f64,
-    b: f64,
-}
-
-impl From<(f64, f64)> for Data {
-    fn from(value: (f64, f64)) -> Self {
-        Self {
-            a: value.0,
-            b: value.1,
-        }
-    }
+    inner: Vec<f64>,
 }
 
 impl Data {
     fn distance(&self, other: &Data) -> f64 {
-        ((self.a - other.a).powi(2) + (self.b - other.b).powi(2)).sqrt()
+        let value: f64 = self
+            .inner
+            .iter()
+            .zip(other.inner.iter())
+            .map(|(a, b)| (a - b).powi(2))
+            .sum();
+
+        value.sqrt()
     }
 }
 
-#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
-enum Label {
-    A,
-    B,
-}
-
+#[derive(Debug)]
 struct SampleData {
     data: Data,
-    label: Label,
+    label: String,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let sample_datas: Vec<_> = vec![
-        ((1.0, 1.1), Label::A),
-        ((1.0, 1.0), Label::A),
-        ((0.0, 0.0), Label::B),
-        ((0.0, 0.1), Label::B),
+        (vec![1.0, 1.1], "A"),
+        (vec![1.0, 1.0], "A"),
+        (vec![0.0, 0.0], "B"),
+        (vec![0.0, 0.1], "B"),
     ]
     .into_iter()
-    .map(|(data, label)| SampleData {
-        data: data.into(),
-        label,
+    .map(|(inner, label)| SampleData {
+        data: Data { inner },
+        label: label.to_string(),
     })
     .collect();
 
-    let res = knn::classify(Data { a: 0.0, b: 0.0 }, &sample_datas, 3);
+    let input = Data {
+        inner: vec![0.0, 0.0],
+    };
+    let res = knn::classify(input, &sample_datas, 3);
     println!("result: {res:?}");
+
+    let file_path = format!(
+        "{}/Work/MachineLearningInActionSourceCode/Ch02/datingTestSet2.txt",
+        dirs::home_dir()
+            .and_then(|hd| hd.to_str().map(|s| s.to_string()))
+            .ok_or(anyhow::anyhow!("can't get home dir"))?
+    );
+
+    let samples = knn::file2matrix(&file_path)?;
+    println!("samples: {samples:?}");
+
+    let licheng_values: Vec<_> = samples.iter().map(|sd| sd.data.inner[0]).collect();
+    let game_values: Vec<_> = samples.iter().map(|sd| sd.data.inner[1]).collect();
+    let ice_cream_values: Vec<_> = samples.iter().map(|sd| sd.data.inner[2]).collect();
+    let labels: Vec<usize> = samples.iter().map(|sd| sd.label.parse().unwrap()).collect();
+
+    let size_arr: Vec<_> = labels.iter().map(|ll| ll * 5).collect();
+    let marker = Marker::new()
+        .size_array(size_arr.clone())
+        .color_array(size_arr);
+
+    let trace1 = Scatter::new(licheng_values, game_values)
+        .mode(Mode::Markers)
+        .marker(marker);
+
+    let mut plt = Plot::new();
+    plt.add_trace(trace1);
+    plt.show();
+
+    Ok(())
 }
