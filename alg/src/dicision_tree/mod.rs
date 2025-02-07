7bee9e27c4046bf275fe6cd99724dd01db848a8f
passchaos <greedwolf.dss@gmail.com>
2025-02-07 20:06:54 +0800

    feat: add split functionality

--- alg/src/dicision_tree/mod.rs
+++ alg/src/dicision_tree/mod.rs
@@ -1,4 +1,4 @@
-use ndarray::ArrayView2;
+use ndarray::{Array2, ArrayView2, Axis, s};
 
 pub fn calculate_shannon_entropy<T: ToString>(data_set: ArrayView2<T>) -> f64 {
     let num_entries = data_set.shape()[0] as f64;
@@ -18,6 +18,25 @@ pub fn calculate_shannon_entropy<T: ToString>(data_set: ArrayView2<T>) -> f64 {
     }
 
     shannon_entropy
+}
+
+pub fn split_data_set<T: Clone + Default + PartialEq>(
+    data_set: ArrayView2<T>,
+    axis: Axis,
+    value: T,
+) -> Array2<T> {
+    let mut ret_data_set = Array2::default([0, data_set.shape()[1] - 1]);
+
+    for row in data_set.rows() {
+        if row[axis.index()] == value {
+            let mut new_vec = row.to_owned();
+            new_vec.remove_index(Axis(0), axis.index());
+
+            ret_data_set.push_row(new_vec.view()).unwrap();
+        }
+    }
+
+    ret_data_set
 }
 
 #[cfg(test)]
@@ -43,5 +62,23 @@ mod tests {
         tracing::info!("entropy: {shannon_entropy}");
 
         // assert_eq!(shannon_entropy, 0.9709505944546686);
+    }
+
+    #[test]
+    fn test_split_data_set() {
+        tracing_subscriber::fmt().init();
+
+        let data_set = array![
+            ["1", "1", "yes"],
+            ["1", "1", "yes"],
+            ["1", "0", "no"],
+            ["0", "1", "no"],
+            ["0", "1", "no"],
+            ["2", "2", "es"]
+        ];
+
+        let ret_data_set = split_data_set(data_set.view(), Axis(0), "1");
+
+        tracing::info!("ret_data_set: {ret_data_set}");
     }
 }
