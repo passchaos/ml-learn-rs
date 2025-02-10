b8422aa1a4e39c33845c33e25e3f36d40d8c80f1
passchaos <greedwolf.dss@gmail.com>
2025-02-10 21:15:00 +0800

    feat: minor changes

--- alg/src/dicision_tree/mod.rs
+++ alg/src/dicision_tree/mod.rs
@@ -1,17 +1,17 @@
 use std::{
     collections::{HashMap, HashSet},
-    fmt::Display,
+    fmt::{Debug, Display},
     hash::Hash,
 };
 
-use ndarray::{Array2, ArrayView2, Axis, s};
+use ndarray::{Array1, Array2, ArrayView1, ArrayView2, Axis, IndexLonger, s};
 
-pub fn calculate_shannon_entropy<T: ToString>(data_set: ArrayView2<T>) -> f64 {
-    let num_entries = data_set.shape()[0] as f64;
+pub fn calculate_shannon_entropy(data_set: ArrayView2<String>) -> f64 {
+    let num_entries = data_set.len_of(Axis(0)) as f64;
 
     let mut label_counts = std::collections::HashMap::new();
 
-    for row in data_set.rows() {
+    for row in data_set.axis_iter(Axis(0)) {
         let label = row[row.len() - 1].to_string();
         *label_counts.entry(label).or_insert(0) += 1;
     }
@@ -26,28 +26,22 @@ pub fn calculate_shannon_entropy<T: ToString>(data_set: ArrayView2<T>) -> f64 {
     shannon_entropy
 }
 
-pub fn split_data_set<T: Clone + Default + PartialEq>(
-    data_set: ArrayView2<T>,
-    index: usize,
-    value: &T,
-) -> Array2<T> {
+pub fn split_data_set(data_set: ArrayView2<String>, index: usize, value: &str) -> Array2<String> {
     let mut ret_data_set = Array2::default([0, data_set.shape()[1] - 1]);
 
-    for row in data_set.rows() {
+    for row in data_set.axis_iter(Axis(0)) {
         if &row[index] == value {
             let mut new_vec = row.to_owned();
             new_vec.remove_index(Axis(0), index);
 
-            ret_data_set.push_row(new_vec.view()).unwrap();
+            ret_data_set.push(Axis(0), new_vec.view()).unwrap();
         }
     }
 
     ret_data_set
 }
 
-pub fn choose_best_feature_to_split<T: Clone + Eq + Default + Display + Hash + ToString>(
-    data_set: ArrayView2<T>,
-) -> Option<usize> {
+pub fn choose_best_feature_to_split(data_set: ArrayView2<String>) -> Option<usize> {
     // 最后一列当做label
     let num_features = data_set.shape()[1] - 1;
 
@@ -57,8 +51,8 @@ pub fn choose_best_feature_to_split<T: Clone + Eq + Default + Display + Hash + T
     tracing::info!("base entropy: {base_ent}");
 
     for i in 0..num_features {
-        let column = data_set.column(i);
-        let values: HashSet<_> = column.into_iter().collect();
+        let axis_data = data_set.index_axis(Axis(1), i);
+        let values: HashSet<_> = axis_data.into_iter().collect();
 
         let mut new_ent = 0.0;
 
@@ -96,9 +90,55 @@ pub fn choose_best_feature_to_split<T: Clone + Eq + Default + Display + Hash + T
     }
 }
 
+pub fn majority_cnt(class_list: ArrayView1<String>) -> String {
+    let mut map = HashMap::new();
+
+    for class in class_list.iter() {
+        *map.entry(class).or_insert(0) += 1;
+    }
+
+    let value = map.into_iter().max_by_key(|(_a, b)| *b).unwrap();
+    value.0.to_owned()
+}
+
+#[derive(Debug)]
+struct MapValue {
+    map: HashMap<String, MapValue>,
+}
+
+impl From<HashMap<String, MapValue>> for MapValue {
+    fn from(map: HashMap<String, MapValue>) -> Self {
+        Self { map }
+    }
+}
+
+// data_set是2维数组，每一行是一个样本，最后一列是label
+// property是1维数组，每一个元素是一个特征，其长度等于data_set的列数-1
+pub fn create_tree(
+    data_set: ArrayView2<String>,
+    features: ArrayView1<String>,
+) -> HashMap<String, MapValue> {
+    // 排除没有熵减的情况
+    let idx = choose_best_feature_to_split(data_set.clone()).unwrap();
+
+    // data_set
+    // data_set.slice(info)
+
+    // split_data_set(data_set, idx, )
+    let mut map = HashMap::new();
+
+    let mut inner_map = HashMap::new();
+
+    let value = MapValue { map: inner_map };
+
+    map.insert("top".to_string(), value);
+
+    map
+}
+
 #[cfg(test)]
 mod tests {
-    use ndarray::array;
+    use ndarray::{ArrayBase, array};
 
     use super::*;
 
@@ -113,7 +153,8 @@ mod tests {
             ["0", "1", "no"],
             ["0", "1", "no"],
             ["2", "2", "es"]
-        ];
+        ]
+        .mapv(|a| a.to_string());
 
         let shannon_entropy = calculate_shannon_entropy(data_set.view());
         tracing::info!("entropy: {shannon_entropy}");
@@ -132,7 +173,8 @@ mod tests {
             ["0", "1", "no"],
             ["0", "1", "no"],
             ["2", "2", "es"]
-        ];
+        ]
+        .mapv(|a| a.to_string());
 
         let ret_data_set = split_data_set(data_set.view(), 0, &"1");
 
@@ -151,10 +193,47 @@ mod tests {
             ["1", "0", "no"],
             ["0", "1", "no"],
             ["0", "1", "no"],
-        ];
+        ]
+        .mapv(|a| a.to_string());
 
         let idx = choose_best_feature_to_split(data_set.view());
 
         tracing::info!("best index: {idx:?}");
+    }
+
+    #[test]
+    fn test_create_dicision_tree() {
+        tracing_subscriber::fmt().init();
+
+        let data_set = array![
+            ["1", "1", "yes"],
+            ["1", "1", "yes"],
+            ["1", "0", "no"],
+            ["0", "1", "no"],
+            ["0", "1", "no"],
+        ]
+        .map(|a| a.to_string());
+
+        let map = create_tree(data_set.view(), array![].view());
+        tracing::info!("map: {map:?}");
+
+        let a = array![[1, 2, 3], [4, 5, 6]];
+
+        for row in a.rows() {
+            tracing::info!("row: {row}");
+        }
+
+        let idx = 0;
+        for row in a.axis_iter(Axis(idx)) {
+            tracing::info!("axis {idx}: {row}");
+        }
+
+        for lane in a.lanes(Axis(idx)) {
+            tracing::info!("lane {idx}: {lane}");
+        }
+
+        for c in a.columns() {
+            tracing::info!("column: {c}");
+        }
     }
 }
