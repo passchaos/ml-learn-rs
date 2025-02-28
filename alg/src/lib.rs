8a257efb5b02326e7cff8bfbd1dbbd5c07a96d0a
passchaos <greedwolf.dss@gmail.com>
2025-03-01 03:45:12 +0800

    feat: add svd test

--- alg/src/lib.rs
+++ alg/src/lib.rs
@@ -1,3 +1,39 @@
 pub mod bayes;
 pub mod decision_tree;
 pub mod knn;
+
+#[cfg(test)]
+mod tests {
+    use ndarray::*;
+    use ndarray_linalg::*;
+
+    #[test]
+    fn test_svd() {
+        let a = arr2(&[
+            [3.0, 4.0, 3.0],
+            [1.0, 2.0, 3.0],
+            [4.0, 2.0, 1.0],
+            [1.0, 1.0, 1.0],
+        ]);
+
+        let (u, s, vt) = a.svd(true, true).unwrap();
+
+        let u = u.unwrap();
+        let vt = vt.unwrap();
+
+        println!("U: {u} S: {s} Vt: {vt}");
+
+        let (m, n) = a.dim();
+
+        let mut sm = Array2::zeros((m, n));
+
+        for i in 0..std::cmp::min(m, n) {
+            sm[(i, i)] = s[i];
+        }
+
+        println!("sm: {sm}");
+
+        let res = u.dot(&sm).dot(&vt);
+        println!("res: {res}");
+    }
+}
