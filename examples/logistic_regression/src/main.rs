b0385c9b53c60f8a1605ab4a620fe2f8158e0545
passchaos <greedwolf.dss@gmail.com>
2025-03-18 15:19:06 +0800

    feat: add logistic regression visualization

--- examples/logistic_regression/src/main.rs
+++ examples/logistic_regression/src/main.rs
@@ -3,7 +3,8 @@ extern crate openblas_src;
 use std::io::BufRead;
 
 use alg::math::sigmoid;
-use ndarray::Array2;
+use egui_plot::{Legend, PlotPoint, PlotPoints, Points};
+use ndarray::{Array1, Array2, ArrayView2};
 
 fn load_data_set() -> (Array2<f64>, Array2<f64>) {
     let path = tools::full_file_path("Ch05/testSet.txt");
@@ -36,7 +37,7 @@ fn load_data_set() -> (Array2<f64>, Array2<f64>) {
     )
 }
 
-fn gradient_ascent(data_in: Array2<f64>, labels_in: Array2<f64>) -> Array2<f64> {
+fn gradient_ascent(data_in: ArrayView2<f64>, labels_in: ArrayView2<f64>) -> Array2<f64> {
     let n = data_in.shape()[1];
     let alpha = 0.001;
     let max_cycles = 500;
@@ -56,7 +57,85 @@ fn gradient_ascent(data_in: Array2<f64>, labels_in: Array2<f64>) -> Array2<f64> 
 fn main() {
     let (data_in, labels_in) = load_data_set();
 
-    let weights = gradient_ascent(data_in, labels_in);
+    let weights = gradient_ascent(data_in.view(), labels_in.view());
+
+    let mut data = vec![];
+    let mut labels = vec![];
+
+    for (i_data, i_label) in data_in
+        .axis_iter(ndarray::Axis(0))
+        .zip(labels_in.axis_iter(ndarray::Axis(0)))
+    {
+        data.push([i_data[1], i_data[2]]);
+        labels.push(i_label[0]);
+    }
 
     println!("weights: {:?}", weights);
+
+    let weights = weights.index_axis(ndarray::Axis(1), 0).to_vec();
+
+    plot_data(data, labels, weights);
+}
+
+fn plot_data(data: Vec<[f64; 2]>, labels: Vec<f64>, weights: Vec<f64>) {
+    let app = App {
+        weights,
+        data,
+        labels,
+    };
+
+    eframe::run_native(
+        "Plot",
+        eframe::NativeOptions::default(),
+        Box::new(|cc| Ok(Box::new(app))),
+    )
+    .unwrap();
+}
+
+struct App {
+    weights: Vec<f64>,
+    data: Vec<[f64; 2]>,
+    labels: Vec<f64>,
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            let legend = Legend::default().position(egui_plot::Corner::RightTop);
+
+            let data = self
+                .data
+                .clone()
+                .into_iter()
+                .zip(self.labels.clone().into_iter());
+
+            egui_plot::Plot::new("plot")
+                .legend(legend)
+                .show(ui, |plot_ui| {
+                    let line_data: Vec<_> = ndarray::linspace(-3.0, 3.0, 100)
+                        .map(|x| {
+                            let y = -(self.weights[0] + self.weights[1] * x) / self.weights[2];
+
+                            [x, y]
+                        })
+                        .collect();
+
+                    let line = egui_plot::Line::new(PlotPoints::from(line_data))
+                        .color(egui::Color32::GREEN);
+                    plot_ui.line(line);
+
+                    for (point, label) in data {
+                        let points = PlotPoints::from(point);
+
+                        let radius = if label == 1.0 { 2.0 } else { 5.0 };
+
+                        plot_ui.points(
+                            Points::new(points)
+                                .radius(radius)
+                                .color(egui::Color32::LIGHT_RED),
+                        );
+                    }
+                });
+        });
+    }
 }
